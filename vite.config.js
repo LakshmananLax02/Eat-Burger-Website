6ed923d4c20fcde6d62616daff8a8b78117be7ce
LakshmananLax02 <lakshmanan02731@gmail.com>
2025-10-16 21:31:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/Eat-Burger-Website/'
+  base: '/Eat-Burger-Website/'
 })
